77893322bc3dd08f2fb0bc9a3127a1dd11fb18aa
BraveBi <2649538083@qq.com>
2022-11-09 23:19:46 +0800

    sso单点登录系统

---
+++ sso-client1/src/main/java/com/example/sso/client/SsoClient1Application.java
@@ -0,0 +1,16 @@
+package com.example.sso.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SSO-客户端1-Demo启动类
+ */
+@SpringBootApplication
+public class SsoClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClient1Application.class, args);
+    }
+
+}
---
+++ sso-client2/src/main/java/com/example/sso/client2/SsoClient2Application.java
@@ -0,0 +1,16 @@
+package com.example.sso.client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SSO-客户端1-Demo启动类
+ */
+@SpringBootApplication
+public class SsoClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClient2Application.class, args);
+    }
+
+}
